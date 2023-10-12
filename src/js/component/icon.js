2d9f103a799c5d22d74237b274a8fb43486f979a
gokaybalkaya <130371393+gokaybalkaya@users.noreply.github.com>
2023-10-12 15:24:07 +0000

    counter done

---
+++ src/js/component/icon.js
@@ -0,0 +1,6 @@
+import fontawesome from '@fontawesome/fontawesome';
+import faClock from '@fontawesome/fontawesome-free-regular/faClock';
+fontawesome.config={
+    autoReplaceSvg: 'nest'
+};
+fontawesome.library.add(faClock);
